ff7cb74fabdac939339e707eab4f2a172f7be86e
sangamthapa23 <sangamthapa9622@gmail.com>
2023-09-09 18:42:12 +0530

    change in array.java file

--- src/Array.java
+++ src/Array.java
@@ -1,6 +1,6 @@
 public class Array{
     public static void main(String[] args) {
-        int D[][]= new int[4][9];
+        int D[][]= new int[4][5];
         int i,j,k=0;
         for(i=0; i<4;i++){
             for(j=0;j<5; j++){
